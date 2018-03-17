1669ce4c178aa589e180b72052b609d353840e21
Robert Jakob <rj@robertjakob.de>
2018-03-17 11:29:50 +0100

    Fixed code (standard linter) and added node_modules to gitignore.

--- gdax_connector/src/index.js
+++ gdax_connector/src/index.js
@@ -1,79 +1,87 @@
-const GDAX = require('gdax');
-const amqp = require('amqplib/callback_api');
+const GDAX = require('gdax')
+const amqp = require('amqplib/callback_api')
 
-console.log("Starting up GDAX connector...");
+console.log('Starting up GDAX connector...')
 
-amqp.connect('amqp://messaging', registerExchangePerProduct);
+amqp.connect('amqp://messaging', registerExchangePerProduct)
 
-function registerExchangePerProduct(err, message_conn) {
+function registerExchangePerProduct (err, messageConn) {
+  if (err) {
+    console.error(err)
+    return
+  }
 
+  getProducts(function (err, products) {
     if (err) {
-        console.error(err);
-        return;
+      console.error(err)
+      return
     }
 
-    getProducts(function(err, products) {
+    messageConn.createChannel(function (err, channel) {
+      if (err) {
+        console.error(err)
+        return
+      }
 
-        message_conn.createChannel(function(err, channel) {
-            for (var i = 0; i < products.length; i++) {
-                var name = "GDAX-" + products[i];
-                channel.assertExchange(name, 'fanout', {durable: false});
-            }
+      for (var i = 0; i < products.length; i++) {
+        var name = 'GDAX-' + products[i]
+        channel.assertExchange(name, 'fanout', {durable: false})
+      }
 
-            createWebsocketAndRun(products, channel);
-        });
-    });
+      createWebsocketAndRun(products, channel)
+    })
+  })
 }
 
-function createWebsocketAndRun(products, channel) {
-    var gdax_conn = createGDAXWebsocketClient(products);
+function createWebsocketAndRun (products, channel) {
+  var websocket = createGDAXWebsocketClient(products)
 
-   const websocketCallback = (data) => {
-       if (data.type == 'ticker') {
-           var msg = data.product_id + ' ' + data.price;
-           channel.publish(data.product_id, '', new Buffer(data.price));
-           console.log(msg);
-       }
-   }
-   
-   websocket.on('message', websocketCallback);
+  const websocketCallback = (data) => {
+    if (data.type === 'ticker') {
+      var msg = 'GDAX-' + data.product_id + ' ' + data.price
+      channel.publish(data.product_id, '', Buffer.from(data.price))
+      console.debug(msg)
+    }
+  }
+
+  websocket.on('message', websocketCallback)
 }
 
-function createGDAXWebsocketClient(products) {
-    const passPhrase = process.env.GDAX_CONNECTOR_PASSPHRASE
-    const apiKey = process.env.GDAX_CONNECTOR_API_KEY;
-    const base64secret = process.env.GDAX_CONNECTOR_BASE64_SECRET;
-    const apiURI = process.env.GDAX_CONNECTOR_API_URL;
+function createGDAXWebsocketClient (products) {
+  const passPhrase = process.env.GDAX_CONNECTOR_PASSPHRASE
+  const apiKey = process.env.GDAX_CONNECTOR_API_KEY
+  const base64secret = process.env.GDAX_CONNECTOR_BASE64_SECRET
+  // const apiURI = process.env.GDAX_CONNECTOR_API_URL
 
-    const websocket = new GDAX.WebsocketClient(
-        products,
-        'wss://ws-feed.gdax.com',
-        {
-            key: apiKey,
-            secret: base64secret,
-            passphrase: passPhrase,
-        },
-        { channels: ['ticker'] }
-    );
+  const websocket = new GDAX.WebsocketClient(
+    products,
+    'wss://ws-feed.gdax.com',
+    {
+      key: apiKey,
+      secret: base64secret,
+      passphrase: passPhrase
+    },
+    { channels: ['ticker'] }
+  )
 
-    return websocket;
+  return websocket
 }
 
-function getProducts(callback) {
-    const publicClient = new GDAX.PublicClient();
-    
-    publicClient.getProducts(function(error, response, data) {
-        if (error) {
-            callback(error, null);
-            return;
-        }
+function getProducts (callback) {
+  const publicClient = new GDAX.PublicClient()
 
-        var products = new Array();
+  publicClient.getProducts(function (error, response, data) {
+    if (error) {
+      callback(error, null)
+      return
+    }
 
-        for (var i = 0; i < data.length; i++) {
-            products.push(data[i].id);
-        }
+    var products = []
 
-        callback(null, products);
-    });
+    for (var i = 0; i < data.length; i++) {
+      products.push(data[i].id)
+    }
+
+    callback(null, products)
+  })
 }
